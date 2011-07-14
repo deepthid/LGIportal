e10d00bdf35be2198c298822dc218d1bb44cd3b3
Deepthi Akkoorath <deepthid@nikhef.nl>
2011-07-14 17:40:15 +0200

    with jobsubmission deletion features

--- lgi/php/logout.php
+++ lgi/php/logout.php
@@ -9,7 +9,6 @@ $dwoo = new Dwoo();
    session_start();
    session_destroy();
    header("Location: ../index.php");
-   //$dwoo->output('../dwoo/logout.tpl', new Dwoo_Data());
-   //$dwoo->output('login.tpl',new Dwoo_Data());
+  ;
 ?>
 
